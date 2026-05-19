130bd368357ef48f9e1347c17e4f9556dd0f06c6
BANKS <bankoles713@gmail.com>
2026-05-19 09:06:38 +0100

    apiService update

--- services/auth/authService.js
+++ services/auth/authService.js
@@ -4,14 +4,14 @@ import apiClient from '@/services/api/apiClient';
 // REGISTER USER
 // =====================
 export const registerUser = async (data) => {
-  const res = await apiClient.post('/register', data);
-  return res.data; // IMPORTANT: return backend data only
+  const res = await apiClient.post('/auth/register', data);
+  return res.data;
 };
 
 // =====================
 // LOGIN USER
 // =====================
 export const loginUser = async (data) => {
-  const res = await apiClient.post('/login', data);
-  return res.data; // IMPORTANT: return backend data only
+  const res = await apiClient.post('/auth/login', data);
+  return res.data;
 };
